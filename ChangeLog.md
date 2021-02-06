1eb0dae029c5cd5b80b6ce0f0b301742f5fc850d
shoshin <shoshinprograms@gmail.com>
2021-02-07 01:33:43 +1100

    First commit, Implementing until Return Values except the exercises

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for schemeIn48Hrs
+
+## Unreleased changes
---
+++ src/Lib.hs
@@ -0,0 +1,59 @@
+module Lib
+    ( someFunc
+    ) where
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+	Left err -> "No match: " ++ show err
+	Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal 
+    = Atom String
+    | List [LispVal]
+    | DottedList [LispVal] LispVal
+    | Number Integer
+    | String String
+    | Bool Bool
+-- Constructors and types have different namespaces, so you can have both a constructor named String and a type named String. Both types and constructor tags always begin with capital letters.
+
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+
+someFunc :: IO ()
+someFunc = do
+	(expr:_) <- getArgs
+	putStrLn (readExpr expr)
+
