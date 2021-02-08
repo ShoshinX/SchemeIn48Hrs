475a38d74b7a607dc79cb5daeaca4b868bfff35a
shoshin <shoshinprograms@gmail.com>
2021-02-08 11:08:20 +1100

    Finished up Evaluation, found a bug with evaluator and string eval

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,18 @@
 module Lib
     ( someFunc
+    , readExpr
     ) where
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Parser (parseExpr)
+import Evaluator (showVal, eval)
+import Common
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+    Left err -> String $ "No match: " ++ show err
+    Right val ->  val
 
 someFunc :: IO ()
-someFunc = do
-    (expr:_) <- getArgs
-    putStrLn (readExpr expr)
-
+someFunc = getArgs >>= print . eval . readExpr . head
